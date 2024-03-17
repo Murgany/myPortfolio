b69889ce7450a4da6e6d43f4db9de2635ec5b797
Murgany <76653393+Murgany@users.noreply.github.com>
2024-03-17 11:45:58 +0200

    Update content_option.js

--- src/content_option.js
+++ src/content_option.js
@@ -116,15 +116,16 @@ const dataportfolio = [
         link: "https://medecgulf.com",
         tech: "Django, HTML, CSS, JavaScript, Ajax, JQuery, Bootstrap"
     },
-    {
-        img: "img/church-website.png",
-        title: "Church Media Distribution Platform",
-        desctiption: "An online platform designed to provide access to audio sermons and PDF books to the congregation in Egypt. The website serves as a digital library where members can access a wide range of religious resources that help them deepen their faith and understanding of the Bible. The website is user-friendly and easy to navigate, with clear categories for sermons and books. Members can stream or download audio sermons and read or download PDF.",
-        link: "https://frpakhomius.com/",
-        tech: "Django, HTML, CSS, JavaScript, Ajax, JQuery, Bootstrap"
-    },
 
 ];
+
+    // {
+    //     img: "img/church-website.png",
+    //     title: "Church Media Distribution Platform",
+    //     desctiption: "An online platform designed to provide access to audio sermons and PDF books to the congregation in Egypt. The website serves as a digital library where members can access a wide range of religious resources that help them deepen their faith and understanding of the Bible. The website is user-friendly and easy to navigate, with clear categories for sermons and books. Members can stream or download audio sermons and read or download PDF.",
+    //     link: "https://frpakhomius.com/",
+    //     tech: "Django, HTML, CSS, JavaScript, Ajax, JQuery, Bootstrap"
+    // },
 
 const contactConfig = {
     YOUR_EMAIL: "rawymo@outlook.com",
